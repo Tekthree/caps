6a76a99f0eba85b9c2938a34b9dfc70b789a7f4f
tekthree <tekthree.gmail.com>
2021-06-30 08:30:43 -0700

    everything is functional

--- vendor/index.js
+++ vendor/index.js
@@ -1,2 +1,35 @@
+'use strict';
+
+
+// -------------- requires and imports --------------------//
 const io = require('socket.io-client');
+const url = 'http://localhost:3000/caps';
 const server = io.connect(url);
+const faker = require('faker');
+
+// ---------------- orders constructor -------------------//
+class Vendor{
+  constructor(){
+    this.array = [];
+  };
+
+
+  create(){
+    let order = {
+      storeName: faker.name.findName(),
+      orderId: faker.datatype.uuid(),
+      customerName: faker.name.findName(),
+      address: faker.address.streetAddress()
+    }
+    this.array.push(order);
+    console.log(order);
+    return order;
+  }
+}
+
+server.on('delivered', (order)=>{
+  console.log(`Vendor: thank you for delivering ${order.payload.orderId}`);
+  console.log(`Date-Time: ${new Date()}`);
+  console.log(order);
+});
+
